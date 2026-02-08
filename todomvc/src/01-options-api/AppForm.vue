dc2242340f748666f1d9011db2c55e502160bcf8
Matteo Antony Mistretta <antony.mistretta@gmail.com>
2026-02-08 10:40:51 +0100

    Add Options API example

---
+++ todomvc/src/01-options-api/AppForm.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  props: ['text'],
+}
+</script>
+
+<template>
+  <!-- WARNING: calling the event 'change' will trigger a DOM change event! -->
+  <form @submit.prevent="$emit('submit')">
+    <input
+      type="text"
+      placeholder="What next?"
+      autofocus
+      :value="text"
+      @input="$emit('inputChange', $event.target.value)"
+    />
+    <button :disabled="!text">Add</button>
+  </form>
+</template>
